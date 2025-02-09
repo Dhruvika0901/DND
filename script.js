ca8b6d311b1023bdd2de1fb0e2be420ad51dd4fa
Dhruvika0901 <dhruvikabhatia0901@gmail.com>
2025-02-09 22:00:56 +0530

    login

---
+++ script.js
@@ -0,0 +1,21 @@
+let prism = document.querySelector(".rec-prism");
+
+function showSignup() {
+    prism.style.transform = "translateZ(-125px) rotateY(-90deg)";
+}
+
+function showLogin() {
+    prism.style.transform = "translateZ(-125px)";
+}
+
+function showForgotPassword() {
+    prism.style.transform = "translateZ(-125px) rotateY(-180deg)";
+}
+
+function showContactUs() {
+    prism.style.transform = "translateZ(-125px) rotateY(90deg)";
+}
+
+function showThankYou() {
+    prism.style.transform = "translateZ(-125px) rotateX(-90deg)";
+}
